d715ab8d3023d1430369041ba43f3a099b362115
colossus06 <denizkavuk86@gmail.com>
2023-07-09 02:28:05 +0300

    add: eksctl 2023 setup and validation nginx🟢

--- 14-Kubernetes-Setup-for-Productions/eks/userdata.sh
+++ 14-Kubernetes-Setup-for-Productions/eks/userdata.sh
@@ -1,12 +1,6 @@
-# Provisioning tools for the cluster setup
-
-yum --help &>> /dev/null
-if [ $? -eq 0 ]
-then
-  yum install zip unzip -y
-else
-  apt update && apt install zip unzip -y
-fi
+#!/bin/bash
+sudo apt update
+sudo apt install zip unzip -y
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
@@ -20,11 +14,3 @@ curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.17.9/2020-08-04/
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin
 kubectl version --short --client
-
-
-#!/bin/bash
-
-sudo apt update && apt install zip unzip -y
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
